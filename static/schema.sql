96a8f8d8923036f5b1b8004c1b0f49d0734ac792
JinmingC <19jchen2919@gmail.com>
2024-10-05 10:35:28 -0500

    update schema

--- static/schema.sql
+++ static/schema.sql
@@ -1,5 +1,17 @@
-CREATE TYPE ReviewType AS (replyid INT, authorid INT, content TEXT, created TIMESTAMP, rating INT);
-CREATE TYPE TopicType AS (topicid INT, authorid INT, title TEXT, content TEXT, created TIMESTAMP, rating INT);
+CREATE TYPE ReviewType AS (
+    replyid INT, 
+    authorid INT, 
+    content TEXT, 
+    created TIMESTAMP, 
+    rating INT);
+
+CREATE TYPE TopicType AS (
+    topicid INT, 
+    authorid INT, 
+    title TEXT, 
+    content TEXT, 
+    created TIMESTAMP, 
+    rating INT);
 
 CREATE TABLE Users (
     userid SERIAL PRIMARY KEY,
@@ -10,5 +22,12 @@ CREATE TABLE Page (
     pageid SERIAL PRIMARY KEY,
     gameid INT NOT NULL,
     reviews ReviewType[],
-    topics TopicType[]
-);
+    topics TopicType[]);
+
+CREATE TABLE Reply (
+    replyid INT,
+    authorid INT,
+    content TEXT,
+    created TIMESTAMP,
+    partentid INT,
+    partentTYPE TEXT);
