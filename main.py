2aa60680184bb11ab753d4e20db6b8bae7203e0f
Ezra <shuku010@umn.edu>
2024-10-06 13:30:45 -0500

    add layout for home and profile
    Improved navbar responsiveness, added a footer, progressed on the home page layout, and nearly completed the profile page ,but minor improvements needed.

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 from igdbAPI import *
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
-    return render_template("home.html")
+def home():
+    return render_template('home.html', user_logged_in='user1') #user_logged_in is just for testing the navbar
+
+@app.route('/user/profile')
+def user_profile():
+    return render_template('user_profile.html', active_page='profile')
+
+@app.route('/user/reviews')
+def user_reviews():
+    return render_template('user_reviews.html', active_page='reviews')
+@app.route('/user/settings')
+def user_settings():
+    return render_template('user_settings.html', active_page='settings')
+
 
 
 @app.route('/review/<string:name>')
 def template_review_page(name):
     data = get_game_data(name)
 
-    # TODO: replace the return with the html template with info from data
-    # data.get("coverImageUrl")
-    # data.get("gameModes")
-    # data.get("genres")
-    # data.get("companies")
-    # data.get("playerPerspectives")
-    # data.get("similarGames")
-    # data.get("storyline")
-    # data.get("summary")
-    # data.get("themes")
+    TODO: replace the return with the html template with info from data
+    data.get("coverImageUrl")
+    data.get("gameModes")
+    data.get("genres")
+    data.get("companies")
+    data.get("playerPerspectives")
+    data.get("similarGames")
+    data.get("storyline")
+    data.get("summary")
+    data.get("themes")
     return None
 
 
@@ -30,3 +41,6 @@ def template_review_page(name):
 def template_review_page1(name):
     # TODO: replace the return with the html template
     return None
+
+
+
