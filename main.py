ca87d3dd104c3d9f87ff70b43c023c1259423379
dtbie <biel9436@stthomas.edu>
2024-10-08 23:37:19 -0500

    Add auth0 back

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ app = Flask(__name__)
 
 setup()
 
-#app.secret_key = os.environ['AUTH0_SECRET_KEY']
+app.secret_key = os.environ['AUTH0_SECRET_KEY']
 
-#oauth = OAuth(app)
-#domain = os.environ['AUTH0_DOMAIN']
-#oauth.register(
- #   "auth0",
-  #  client_id=os.environ['AUTH0_CLIENT_ID'],
-   # client_secret=os.environ['AUTH0_SECRET'],
-    #client_kwargs={
-     #   "scope": "openid profile email",
-   # },
+oauth = OAuth(app)
+domain = os.environ['AUTH0_DOMAIN']
+oauth.register(
+    "auth0",
+    client_id=os.environ['AUTH0_CLIENT_ID'],
+    client_secret=os.environ['AUTH0_SECRET'],
+    client_kwargs={
+        "scope": "openid profile email",
+    },
     #server_metadata_url=f'https://{domain}/.well-known/openid-configuration',
-#)
+)
 
 
 @app.route("/")
@@ -52,49 +52,49 @@ def user_settings():
 
 
 # Controllers API
-# TODO change home page to the following 
+ #TODO change home page to the following
 # below is the code from the example downloaded from auth0
-# @app.route("/a")
-# def home():
-#    return render_template(
-#        "home.html",
-#        session=session.get("user"),
-#        pretty=json.dumps(session.get("user"), indent=4),
-#    )
+@app.route("/a")
+def home():
+   return render_template(
+       "home.html",
+       session=session.get("user"),
+       pretty=json.dumps(session.get("user"), indent=4),
+   )
 
 
-#@app.route("/callback", methods=["GET", "POST"])
-#def callback():
-#    token = oauth.auth0.authorize_access_token()
-#    session["user"] = token
-#    print(token)
-#    return redirect("/")
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    print(token)
+    return redirect("/")
 
 
-#@app.route("/login")
-#def login():
-#    return oauth.auth0.authorize_redirect(
-#        redirect_uri=url_for("callback", _external=True)
-#    )
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
 
 
-#@app.route("/logout")
-#def logout():
- #   session.clear()
-  #  return redirect(
-   #     "https://"
-    #    + domain
-     #   + "/v2/logout?"
-      #  + urlencode(
-       #     {
-        #        # replace hello_world with actual function for homepage endpoint
-         #       "returnTo": url_for("hello_world", _external=True),
-          #      "client_id": os.environ['AUTH0_CLIENT_ID'],
-           # },
-            #quote_via=quote_plus,
-       # )
-    #)
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + domain
+        + "/v2/logout?"
+        + urlencode(
+            {
+                # replace hello_world with actual function for homepage endpoint
+                "returnTo": url_for("hello_world", _external=True),
+                "client_id": os.environ['AUTH0_CLIENT_ID'],
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 
 @app.route('/review/<string:id>')
